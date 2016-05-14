6f0f499a6979708638010a5668dfe4417d33c95d
MiMa <michalnebbia@gmail.com>
2016-05-14 22:40:08 +0200

    removed unnecesary code

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,8 +1,3 @@
-import '../../api/lists/methods.js';
-import '../../api/lists/server/publications.js';
-import '../../api/todos/methods.js';
-import '../../api/todos/server/publications.js';
-
 import '../../api/lections/methods.js';
 import '../../api/lections/server/publications.js';
 import '../../api/exercises/methods.js';
