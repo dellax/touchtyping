78f96d0b1c8ff9d64edd8cfa19220cf2b94e6ae7
MiMa <michalnebbia@gmail.com>
2016-05-19 09:54:02 +0200

    added initial data for cars

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Lections } from '../../api/lections/lections.js';
 import { Exercises } from '../../api/exercises/exercises.js';
+import { Models } from '../../api/models/models.js';
+import { def0, def1, def2, def3, dodge, jeep, lambo, police } from './car-models.js';
 
 // if the database is empty on server start, create some sample data.
 // TODO ...
@@ -31,10 +33,6 @@ Meteor.startup(() => {
         Roles.addUsersToRoles(id, user.roles);
       }
     });
-
-    const models = [
-      { name: 'Model zakladny', image: '', points: 0 },
-    ];
 
     const data = [
       {
@@ -96,6 +94,28 @@ Meteor.startup(() => {
 
         timestamp += 1; // ensure unique timestamp.
       });
+    });
+
+    const models = [
+      { name: 'Model zakladny', image: def0, points: 0 },
+      { name: 'Model zakladny', image: def1, points: 0 },
+      { name: 'Model zakladny', image: def2, points: 0 },
+      { name: 'Model zakladny', image: def3, points: 0 },
+      { name: 'Model dodge', image: dodge, points: 300 },
+      { name: 'Model jeep', image: jeep, points: 500 },
+      { name: 'Model lamborghini', image: lambo, points: 670 },
+      { name: 'Model police', image: police, points: 1000 }
+    ];
+
+    models.forEach((model) => {
+      Models.insert({
+        name: model.name,
+        image: model.image,
+        points: model.points,
+        createdAt: new Date(timestamp)
+      });
+
+      timestamp += 1;
     });
   }
 });
