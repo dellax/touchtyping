2bc6405e7084306f21f63f916097330b8ae3b33d
MiMa <michalnebbia@gmail.com>
2016-05-31 00:34:51 +0200

    updated statistics page

--- imports/ui/pages/StatisticsPage.jsx
+++ imports/ui/pages/StatisticsPage.jsx
@@ -3,6 +3,10 @@ import NotFoundPage from '../pages/NotFoundPage.jsx';
 import Message from '../components/Message.jsx';
 import Statistics from '../components/TouchType/Statistics.jsx';
 import Modal from 'boron/DropModal';
+import 'moment/locale/sk';
+import moment from 'moment';
+
+moment.locale('sk');
 
 
 export default class StatisticsPage extends React.Component {
@@ -42,11 +46,31 @@ export default class StatisticsPage extends React.Component {
       <div>
         <div className="statistics-page">
           <h2>Štatistiky</h2>
-          {statisticWithExercises.map((o) => {
-            return (
-              <div className="statistics-item" onClick={this.handleOnClick.bind(this,o)}>{o.exercise.name}</div>
-            )
-          })}
+          <p>Po kliknuťí na niektorú zo štatistík sa zobrazí detailnejší nahľad.</p>
+          
+          <table id="hor-minimalist-a">
+            <thead>
+              <tr>
+                <th scope="col">Dátum</th>
+                <th scope="col">Názov cvičenia</th>
+                <th scope="col">Body</th>
+                <th scope="col">WPM</th>
+              </tr>
+            </thead>
+            <tbody>
+              {statisticWithExercises.map((o) => {
+                return (
+                  <tr onClick={this.handleOnClick.bind(this, o)}>
+                    <td>{moment(o.statistics.createdAt).format('llll')}</td>
+                    <td>{o.exercise.name}</td>
+                    <td>{o.exercise.points}</td>
+                    <td>{o.statistics.averageWpm}</td>
+                  </tr>
+                )
+              })}
+              
+            </tbody>
+          </table>
 
         </div>
         <Modal ref="modal" modalStyle={modalStyle}>
